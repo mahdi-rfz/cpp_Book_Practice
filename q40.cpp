d34f2853b54250f3b06fa00519d0ea682141cc56
mahdi-rfz <mahdifeyzolahy@gmail.com>
2024-08-01 14:21:19 +0330

    q40

---
+++ q40.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    for(int counter = 1;counter<6;counter++){
+        cout << "your number is " << counter << endl;
+    }
+    return 0;
+}
