3201420e6751194b7812a26713bff5b4434f8c3a
mahdi-rfz <mahdifeyzolahy@gmail.com>
2024-08-04 21:47:22 +0330

    q63

---
+++ q63.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int ascii_start = 97;
+    int ascii_finish = 122;
+
+    while(true){
+        cout << ascii_start - 32 << endl;
+
+        if(ascii_start == ascii_finish){
+            break;
+        }
+        ascii_start = ascii_start + 1;
+    }
+}
