80f249bbc3fd92dfc01d0e02ee90ef6a45c6e089
mahdi-rfz <mahdifeyzolahy@gmail.com>
2024-08-04 17:01:35 +0330

    q64

---
+++ q64.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int num1 = 0;
+    int num2 = 1;
+    int cache_number;
+    int counter = 0;
+
+    cout << num1 << endl;
+    cout << num2 << endl;
+
+    while(true){
+        cache_number = num1 + num2;
+        cout << cache_number << endl;
+
+        num1 = num2;
+        num2 = cache_number;
+
+        counter = counter + 1;
+
+        if(counter == 7){
+            break;
+            }
+    }
+}
