f540dcc92c3ac154b77698b7e10ec263582f4862
mahdi-rfz <mahdifeyzolahy@gmail.com>
2024-08-07 10:58:09 +0330

    fix /0 bug

--- q56.cpp
+++ q56.cpp
@@ -7,16 +7,17 @@ int main(){
     cout << "Enter your number : ";
     cin >> number;
 
-    int counter = 0;
+    int counter = 1;
     int cache;
     while(true){
         cache = number / counter;
-        if(cache*counter == number){
-            cout << counter;
+        if(cache * counter == number){
+            cout << counter << endl;
         }
         if(counter == number + 1){
             break;
         }
+        counter = counter + 1;
     }
     
 }
