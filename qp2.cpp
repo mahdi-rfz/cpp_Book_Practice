b2972984e0b7a3ac6c5a0f0649216fc3063193b2
mahdi-rfz <mahdifeyzolahy@gmail.com>
2024-08-08 09:55:00 +0330

    personal q 2

---
+++ qp2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int code; 
+    cout << "Enter assci code : ";
+    cin >> code;
+
+    char character = code;
+
+    cout << "Character : " << character << endl;
+
+    return 0;
+}
