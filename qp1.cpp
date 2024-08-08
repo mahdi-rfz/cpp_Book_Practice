2871d20f11da6cb73ca1ddeb955b94c05d7b33c0
mahdi-rfz <mahdifeyzolahy@gmail.com>
2024-08-08 09:54:33 +0330

    personal q 1

---
+++ qp1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    char x;
+    cout << "Enter your character : ";
+    cin >> x;
+    cout << endl <<  int(x) << endl;
+
+    return 0;
+}
